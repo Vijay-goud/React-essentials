0cb57818208b4ec0fd1d76df7e905973577e02bd
Vijay-goud <vijay.goud.ce@gmail.com>
2023-12-30 20:54:50 +0530

    React essentials

---
+++ 01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton({ children , onSelect, isSelected ,...props}) {
+  return (
+    <li>
+      <button className={isSelected ? 'active': undefined} onClick={onSelect} {...props}>{children}</button>
+    </li>
+  );
+}
